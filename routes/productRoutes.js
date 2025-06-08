37c5865e6917e648cf92523ad4ec598505b9720c
Inima <inima@gmail.com>
2025-06-08 08:55:20 +0545

    changes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,14 +4,14 @@ import { fileCheck, updateFileCheck } from "../middlewares/checkFile.js";
 import { notAllowed } from "../utils/shareFunc.js";
 import { checkId } from "../middlewares/checkId.js";
 import { productValSchema, validates } from "../utils/validator.js";
-import { userCheck } from "../middlewares/userCheck.js";
+import { adminCheck, userCheck } from "../middlewares/userCheck.js";
 
 const router = express.Router();
 
-router.route('/').get(getProducts).post(userCheck, validates.body(productValSchema), fileCheck, addProducts).all(notAllowed);
+router.route('/').get(getProducts).post(userCheck, adminCheck, validates.body(productValSchema), fileCheck, addProducts).all(notAllowed);
 
 router.route('/top-5').get(getTop5, getProducts).all(notAllowed);
 
-router.route('/:id').get(getProduct).patch(checkId, updateFileCheck, updateProduct).delete(checkId, removeProduct).all(notAllowed);
+router.route('/:id').get(getProduct).patch(userCheck, adminCheck, checkId, updateFileCheck, updateProduct).delete(userCheck, adminCheck,checkId, removeProduct).all(notAllowed);
 
 export default router;
